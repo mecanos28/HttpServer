60abe5ed20b784631e69ecdad40e8a152a4fc15b
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2018-03-27 18:44:38 -0600

    Primer commit, inicio del Servidor que escucha y crea threads y lo primero de la implementación de cada uno. Falta ver como adquirimos el archivo, hacer casos por los tipos de requests, ver si sirve lo del bufferReader para las pruebas, etc...

--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,25 @@
-import com.sun.xml.internal.bind.v2.TODO;
-
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.IOException;
 
 /**
  * Created by feroj_000 on 27/3/2018.
  */
+
+//TODO: Crear otra clase que sea como una consola en donde se inicie el servidor que tenga una instancia de esta clase
+
 public class Server {
-    private ServerSocket serverListener;
-    public int port;
+    protected ServerSocket serverListener;
+    protected int port;
 
     Server (int port){this.port=port;} //Constructor
     public void listen() throws IOException {
         serverListener = new ServerSocket(port);
         Socket requester = null;
-
         while(true){
             requester = serverListener.accept(); //se acepta el nuevo cliente al servidor
-            Thread thread = new Thread(new ...); //TODO para cada thread crear un nuevo objeto de la clase que maneja requestst http
+            Thread thread = new Thread( new requestHandler(requester) ); //cada request es un hilo nuevo
             thread.start();
-    }
-
-
-
+        }
     }
 }
